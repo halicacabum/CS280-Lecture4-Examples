c8dd0255f2d34e3777b166c49b1d47cb800b81b0
halicacabum <halicacabum@gmail.com>
2017-03-14 20:45:14 +0800

    迴圈練習

---
+++ Example01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Status response = Status.Success;
+
+            switch (response)
+            {
+                case Status.Success:
+                    Console.WriteLine("狀態是成功");
+                    Console.WriteLine(response);
+                    break;
+
+                case Status.Error:
+                    Console.WriteLine("狀態是錯誤");
+                    Console.WriteLine(response);
+                    break;
+
+                default:
+                    Console.WriteLine("狀態不明");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
